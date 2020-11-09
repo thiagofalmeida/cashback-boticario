7691c418cea6e04c717f4686d8a12aa1f23b1a9f
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-11-09 20:57:57 -0300

    Basic configurations

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import routes from './shared/http/routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server Started on port 3333!');
+});
