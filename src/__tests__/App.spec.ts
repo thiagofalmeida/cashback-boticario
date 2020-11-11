4564c4630e62e0cdb87eab537eb590fa911dce80
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-11-11 10:26:01 -0300

    Tests for users and sessions services

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -54,18 +54,82 @@ describe('App', () => {
       password: '123123123',
     });
 
-    expect(user.body).toEqual(
-      expect.objectContaining({
-        cpf: '08045698742',
-        email: 'acrico@testes.com.br',
-      }),
-    );
-
-    const response = await request(app).post('/users').send({
-      cpf: '08045698742',
-      email: 'acrico@testes.com.br',
+    const userTwo = await request(app).post('/users').send({
+      name: 'Josberanilson Acricocrildo',
+      cpf: user.body.cpf,
+      email: user.body.email,
+      password: '123123123',
     });
 
-    expect(response.status).toBe(400);
+    expect(userTwo.status).toBe(400);
+  });
+
+  it('should not be able to authenticate a non-existent user', async () => {
+    const session = await request(app).post('/sessions').send({
+      email: 'non_existent_user@email.com',
+      password: 'wrong-password',
+    });
+
+    expect(session.status).toBe(401);
+  });
+
+  it('should not be able to authenticate a user with wrong credentials', async () => {
+    const user = await request(app).post('/users').send({
+      name: 'Acricocrildo Josberanilson',
+      cpf: '08045698742',
+      email: 'acrico@testes.com.br',
+      password: '123123123',
+    });
+
+    const session = await request(app).post('/sessions').send({
+      email: user.body.email,
+      password: 'wrong-password',
+    });
+
+    expect(session.status).toBe(401);
+  });
+
+  it('should be able to list the current cashback credit accumulation from a user', async () => {
+    const user = await request(app).post('/users').send({
+      name: 'Acricocrildo Josberanilson',
+      cpf: '08045698742',
+      email: 'acrico@testes.com.br',
+      password: '123123123',
+    });
+
+    const session = await request(app).post('/sessions').send({
+      email: user.body.email,
+      password: '123123123',
+    });
+
+    const cashback = await request(app)
+      .get('/cashback?cpf=08045698742')
+      .set('Authorization', `Bearer ${session.body.token}`);
+
+    expect(cashback.body).toEqual(
+      expect.objectContaining({
+        cpf: '08045698742',
+      }),
+    );
+  });
+
+  it('should not be able to list the current cashback credit accumulation from a non-existent user', async () => {
+    const user = await request(app).post('/users').send({
+      name: 'Acricocrildo Josberanilson',
+      cpf: '08045698742',
+      email: 'acrico@testes.com.br',
+      password: '123123123',
+    });
+
+    const session = await request(app).post('/sessions').send({
+      email: user.body.email,
+      password: '123123123',
+    });
+
+    const cashback = await request(app)
+      .get('/cashback?cpf=123132132')
+      .set('Authorization', `Bearer ${session.body.token}`);
+
+    expect(cashback.status).toBe(400);
   });
 });
